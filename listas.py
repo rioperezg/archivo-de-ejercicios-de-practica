9713d16d1562dc20b4ad15a5aad1293814afa625
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-10-26 20:14:53 +0200

    other commit

---
+++ listas.py
@@ -0,0 +1,17 @@
+lista = list("python is awesome")
+print(lista)
+#para seleccionar un elemento unico de la lista:
+lista[4]
+lista[-3]
+print("lista[4]=", lista[4])
+print("lista[-3]=", lista[3])
+#extraccion de sublista
+print("lista[:6]=", lista[:6])
+print("lista[6:]=", lista[6:])
+print("lista[-6:]=", lista[-6:])
+print("lista[3:6]=", lista[3:6])
+print("lista[2::7]=", lista[2::7])
+#escritura de un miembro de la lista
+lista[5] = "r"
+print(lista)
+
