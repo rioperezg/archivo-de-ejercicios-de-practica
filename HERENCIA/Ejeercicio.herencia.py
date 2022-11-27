643ed3bf1a5c528746def62cc5fc66de8f2ec0af
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-11-27 17:15:51 +0100

    opeeeera

--- HERENCIA/Ejeercicio.herencia.py
+++ HERENCIA/Ejeercicio.herencia.py
@@ -5,6 +5,14 @@ class Punto:
         self.z = z
     def mostrar(self):
         print("Punto {}, {}, {}".format(self.x, self.y, self.z))
-p = Punto(1, 2, 3)
-p.mostrar()    
-    
+    #FASE2  
+    def modulo(self):
+        return (self.x**2 + self.y**2 + self.z**2)**(1/2)
+    def distancia(self, other=None):
+        if other is None:
+            other = (0, 0, 0)
+        return ((self.x-other.x)**2+(self.y-other.y)**2+(self.z-other.z)**2)**(1/2)
+P = Punto(1, 2, 3)
+P.mostrar()
+print("|P|=", P.modulo())
+print("La distancia entre P y el punto (2, 4, 1) es:", P.distancia())
